1dc6fa98205545e5825d1fd2cc494d85b1d6cbdb
Marvin Ulrich <marvin@infocaster.net>
2021-12-02 20:22:37 +0100

    Day 1

---
+++ Advent2021/Advent2021/Day1.cs
@@ -0,0 +1,41 @@
+﻿namespace Advent2021
+{
+    internal class Day1
+    {
+        public async Task PuzzleOne()
+        {
+            var ups = 0;
+            var input = (await File.ReadAllLinesAsync("Input/Day1_1.txt")).Select(int.Parse).ToList();
+
+            Console.WriteLine($"Number of inputs = {input.Count}");
+            for (var i = 1; i < input.Count; i++)
+            {
+                if (Increased(input[i - 1], input[i]))
+                    ups++;
+            }
+
+            Console.WriteLine($"The amount of ups is {ups}\n");
+        }
+
+        public async Task PuzzleTwo()
+        {
+            var ups = 0;
+            var input = (await File.ReadAllLinesAsync("Input/Day1_1.txt")).Select(int.Parse).ToList();
+
+            Console.WriteLine($"Number of inputs = {input.Count}");
+            for (var i = 3; i < input.Count; i++) //1,2,3 - 0,1,2 || 4,5,6 - 3,4,5
+            {
+                if (Increased(input[i - 3] + input[i - 2] + input[i - 1], input[i - 2] + input[i - 1] + input[i]))
+                    ups++;
+            }
+
+            Console.WriteLine($"The amount of ups is {ups}\n");
+        }
+
+        private bool Increased(int v1, int v2)
+        {
+            Console.WriteLine($"Checking {v2} > {v1}: {v2 > v1}");
+            return v2 > v1;
+        }
+    }
+}
---
+++ Advent2021/Advent2021/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Advent2021;
+
+Console.WriteLine("Hi Advent of code 2021\n");
+
+var day1 = new Day1();
+await day1.PuzzleOne();
+await day1.PuzzleTwo();
+
+Console.ReadLine();
