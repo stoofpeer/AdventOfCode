8de440a5c077908f7bc31b517c6d1b802ccf1c7e
Marvin Ulrich <marvin@infocaster.net>
2021-12-06 19:04:36 +0100

    Day 6

---
+++ Advent2021/Advent2021/Day6.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace Advent2021
+{
+    internal class Day6
+    {
+        public async Task PuzzleOne()
+        {
+            var fishInput = (await File.ReadAllTextAsync("Input/Day6.txt")).Split(',').Select(int.Parse).ToList();
+            for (var day = 1; day <= 80; day++)
+            {
+                var fishEOL = fishInput.Count(x => x == 0);
+                fishInput = fishInput.Select(x => x - 1).ToList();
+                if (fishEOL > 0)
+                {
+                    fishInput.AddRange(Enumerable.Range(0, fishEOL).Select(x => 8));
+                    fishInput.AddRange(Enumerable.Range(0, fishEOL).Select(x => 6));
+                    fishInput.RemoveAll(x => x < 0);
+                }
+
+                Console.WriteLine($"After day {day}, there are {fishInput.Count}");
+            }
+        }
+
+        public async Task PuzzleTwo()
+        {
+            var fishInput = (await File.ReadAllTextAsync("Input/Day6.txt")).Split(',').Select(int.Parse).ToList();
+
+            var timerInterval = new long[9];
+            foreach (var fish in fishInput)
+                timerInterval[fish]++;
+
+            for (var day = 1; day <= 256; day++)
+            {
+                var eolFish = timerInterval[0];
+                timerInterval[0] = 0;
+                timerInterval = Enumerable.Range(0, 9).Select(x => x != 8 ? timerInterval[x + 1] : eolFish).ToArray();
+                timerInterval[6] += eolFish;
+
+                Console.WriteLine($"After day {day}, there are {timerInterval.Sum()}");
+            }
+        }
+    }
+}
--- Advent2021/Advent2021/Program.cs
+++ Advent2021/Advent2021/Program.cs
@@ -3,24 +3,28 @@ using Advent2021;
 
 Console.WriteLine("Hi Advent of code 2021\n");
 
-var day1 = new Day1();
-await day1.PuzzleOne();
-await day1.PuzzleTwo();
+//var day1 = new Day1();
+//await day1.PuzzleOne();
+//await day1.PuzzleTwo();
 
-var day2 = new Day2();
-await day2.PuzzleOne();
-await day2.PuzzleTwo();
+//var day2 = new Day2();
+//await day2.PuzzleOne();
+//await day2.PuzzleTwo();
 
-var day3 = new Day3();
-await day3.PuzzleOne();
-await day3.PuzzleTwo();
+//var day3 = new Day3();
+//await day3.PuzzleOne();
+//await day3.PuzzleTwo();
 
-var day4 = new Day4();
-await day4.PuzzleOne();
-await day4.PuzzleTwo();
+//var day4 = new Day4();
+//await day4.PuzzleOne();
+//await day4.PuzzleTwo();
 
-var day5 = new Day5();
-await day5.PuzzleOne();
-await day5.PuzzleTwo();
+//var day5 = new Day5();
+//await day5.PuzzleOne();
+//await day5.PuzzleTwo();
+
+var day6 = new Day6();
+await day6.PuzzleOne();
+await day6.PuzzleTwo();
 
 Console.ReadLine();
